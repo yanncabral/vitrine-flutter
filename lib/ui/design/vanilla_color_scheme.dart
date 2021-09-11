513aa588d760b31e1daf7a9bcfaf5efba9e458a9
Yann <iamyanndias@gmail.com>
2021-09-11 19:50:59 -0300

    add VanillaColorScheme

---
+++ lib/ui/design/vanilla_color_scheme.dart
@@ -0,0 +1,16 @@
+import 'dart:ui';
+
+// ignore: avoid_classes_with_only_static_members
+class VanillaColorScheme {
+  static Color primary = const Color(0xff6E8CF5);
+  static Color secondary = const Color(0xffB487F5);
+  static Color black = const Color(0xff282332);
+  static Color dark = const Color(0xff413C55);
+  static Color medium = const Color(0xff9191A6);
+  static Color light = const Color(0xffEAEBF1);
+  static Color extraLight = const Color(0xffF6F7FC);
+  static Color white = const Color(0xffFFFFFF);
+  static Color success = const Color(0xff38D49C);
+  static Color error = const Color(0xffF03C6E);
+  static Color warning = const Color(0xffFFCB45);
+}
