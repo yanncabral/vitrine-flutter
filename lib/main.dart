c99b659c01f15ebc4f966516906cd9fbdeac65d9
Yann <iamyanndias@gmail.com>
2021-09-28 13:59:10 -0300

    add todo's in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:vitrine/ui/design/text_theme.dart';
+import 'package:vitrine/ui/onboarding_page/onboarding_page.dart';
 import 'package:vitrine/ui/signin_page.dart';
 
 Future<void> main() async {
@@ -22,18 +23,21 @@ class _AppState extends State<App> {
       title: "Vitrine",
       theme: ThemeData(textTheme: textTheme),
       debugShowCheckedModeBanner: false,
+      routes: {
+        '/auth': (context) => SignInPage(),
+      },
       home: FutureBuilder(
         future: _initialization,
         builder: (context, snapshot) {
           if (snapshot.hasError) {
-            return const Text("error? rs");
+            return const Text("error? rs"); // TODO: Add an error screen
           }
 
           if (snapshot.connectionState == ConnectionState.done) {
-            return SignInPage();
-            // return OnboardingPage();
+            // return SignInPage();
+            return OnboardingPage();
           }
-          return const Text("carregando? rs");
+          return const Text("carregando? rs"); // TODO: Add a loading screen
         },
       ),
     );
