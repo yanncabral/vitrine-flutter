95b94cdd2f2b87016ea9cfaf4c7ca93bdb0fafad
Yann <iamyanndias@gmail.com>
2021-10-01 14:00:25 -0300

    ensure EmailAddress returns an instance if email is valid

--- test/domain/value_objects/email_address_test.dart
+++ test/domain/value_objects/email_address_test.dart
@@ -1,3 +1,4 @@
+import 'package:faker/faker.dart';
 import 'package:test/test.dart';
 import 'package:vitrine/domain/validation/validation_error.dart';
 import 'package:vitrine/domain/validation/validator.dart';
@@ -16,9 +17,16 @@ class EmailAddress extends ValueObject<String> {
 }
 
 void main() {
+  final faker = Faker();
   group("EmailAddress", () {
     test("Should throws ValidationError.invalid if email is invalid", () {
       expect(() => EmailAddress("test"), throwsA(ValidationError.invalid));
+    });
+    test("Should returns an instance if email is valid", () {
+      expect(EmailAddress("yann@gmail.com"), isA<EmailAddress>());
+      expect(EmailAddress(faker.internet.email()), isA<EmailAddress>());
+      expect(EmailAddress(faker.internet.freeEmail()), isA<EmailAddress>());
+      expect(EmailAddress(faker.internet.safeEmail()), isA<EmailAddress>());
     });
   });
 }
