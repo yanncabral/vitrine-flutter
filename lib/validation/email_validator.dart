457f7ea2d506637ca8547ec3f36fc406e451553a
Yann <iamyanndias@gmail.com>
2021-10-01 13:52:36 -0300

    move EmailValidator to its own file

---
+++ lib/validation/email_validator.dart
@@ -0,0 +1,13 @@
+import 'package:email_validator/email_validator.dart'
+    as external_mail_valiadator;
+import 'package:vitrine/domain/validation/validation_error.dart';
+import 'package:vitrine/domain/validation/validator.dart';
+
+class EmailValidator implements Validator<String> {
+  @override
+  ValidationError? call(String value) {
+    return external_mail_valiadator.EmailValidator.validate(value) == true
+        ? null
+        : ValidationError.invalid;
+  }
+}
--- test/validation/email_validator_test.dart
+++ test/validation/email_validator_test.dart
@@ -1,18 +1,7 @@
-import 'package:email_validator/email_validator.dart'
-    as external_mail_valiadator;
 import 'package:faker/faker.dart';
 import 'package:test/test.dart';
 import 'package:vitrine/domain/validation/validation_error.dart';
-import 'package:vitrine/domain/validation/validator.dart';
-
-class EmailValidator implements Validator<String> {
-  @override
-  ValidationError? call(String value) {
-    return external_mail_valiadator.EmailValidator.validate(value) == true
-        ? null
-        : ValidationError.invalid;
-  }
-}
+import 'package:vitrine/validation/email_validator.dart';
 
 void main() {
   final faker = Faker();
