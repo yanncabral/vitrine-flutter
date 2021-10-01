a04a3f9e61e3e964c392d362a0c318860cd9b70b
Yann <iamyanndias@gmail.com>
2021-10-01 14:27:26 -0300

    move Password to its own file

---
+++ lib/domain/value_objects/password.dart
@@ -0,0 +1,18 @@
+import 'package:vitrine/domain/validation/validator.dart';
+import 'package:vitrine/domain/value_objects/value_object.dart';
+import 'package:vitrine/validation/not_empty_validator.dart';
+import 'package:vitrine/validation/too_short_validator.dart';
+
+class Password extends ValueObject<String> {
+  Password(String data) : super(data) {
+    if (validationError != null) {
+      throw validationError!;
+    }
+  }
+
+  @override
+  List<Validator> get validators => [
+        NotEmptyValidator(),
+        TooShortValidator(minimumLenght: 8),
+      ];
+}
--- test/domain/value_objects/password_test.dart
+++ test/domain/value_objects/password_test.dart
@@ -1,23 +1,6 @@
 import 'package:test/test.dart';
 import 'package:vitrine/domain/validation/validation_error.dart';
-import 'package:vitrine/domain/validation/validator.dart';
-import 'package:vitrine/domain/value_objects/value_object.dart';
-import 'package:vitrine/validation/not_empty_validator.dart';
-import 'package:vitrine/validation/too_short_validator.dart';
-
-class Password extends ValueObject<String> {
-  Password(String data) : super(data) {
-    if (validationError != null) {
-      throw validationError!;
-    }
-  }
-
-  @override
-  List<Validator> get validators => [
-        NotEmptyValidator(),
-        TooShortValidator(minimumLenght: 8),
-      ];
-}
+import 'package:vitrine/domain/value_objects/password.dart';
 
 void main() {
   group("Password", () {
