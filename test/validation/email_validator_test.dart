efe7804919d3821502d645dda8e4a93c56b56e2c
Yann <iamyanndias@gmail.com>
2021-10-01 13:49:57 -0300

    ensure EmailValidator returns null if email is valid

--- test/validation/email_validator_test.dart
+++ test/validation/email_validator_test.dart
@@ -1,3 +1,6 @@
+import 'package:email_validator/email_validator.dart'
+    as external_mail_valiadator;
+import 'package:faker/faker.dart';
 import 'package:test/test.dart';
 import 'package:vitrine/domain/validation/validation_error.dart';
 import 'package:vitrine/domain/validation/validator.dart';
@@ -5,11 +8,14 @@ import 'package:vitrine/domain/validation/validator.dart';
 class EmailValidator implements Validator<String> {
   @override
   ValidationError? call(String value) {
-    return ValidationError.invalid;
+    return external_mail_valiadator.EmailValidator.validate(value) == true
+        ? null
+        : ValidationError.invalid;
   }
 }
 
 void main() {
+  final faker = Faker();
   group("EmailValidator", () {
     test("Should returns a ValidationError if email is invalid", () {
       final sut = EmailValidator();
@@ -21,6 +27,13 @@ void main() {
       expect(sut(".abc@mail.com"), ValidationError.invalid);
       expect(sut("abc#def@mail.com"), ValidationError.invalid);
       expect(sut("@mail.com"), ValidationError.invalid);
+    });
+    test("Should returns null if email is valid", () {
+      final sut = EmailValidator();
+      expect(sut("yann@gmail.com"), null);
+      expect(sut(faker.internet.email()), null);
+      expect(sut(faker.internet.freeEmail()), null);
+      expect(sut(faker.internet.safeEmail()), null);
     });
   });
 }
