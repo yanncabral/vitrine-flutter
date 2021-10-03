08db8a1d2ba1ab403472a3f581eff744e7ab1a96
Yann <iamyanndias@gmail.com>
2021-10-03 14:27:37 -0300

    ensure if validators is actually callded

--- test/domain/value_objects/email_address_test.dart
+++ test/domain/value_objects/email_address_test.dart
@@ -1,19 +1,28 @@
 import 'package:faker/faker.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
-import 'package:vitrine/domain/validation/validation_error.dart';
 import 'package:vitrine/domain/value_objects/email_address.dart';
+import 'package:vitrine/validation/email_validator.dart';
+
+class EmailValidatorMock extends Mock implements EmailValidator {}
 
 void main() {
   final faker = Faker();
   group("EmailAddress", () {
-    test("Should throws ValidationError.invalid if email is invalid", () {
-      expect(() => EmailAddress("test"), throwsA(ValidationError.invalid));
-    });
-    test("Should returns an instance if email is valid", () {
-      expect(EmailAddress("yann@gmail.com"), isA<EmailAddress>());
-      expect(EmailAddress(faker.internet.email()), isA<EmailAddress>());
-      expect(EmailAddress(faker.internet.freeEmail()), isA<EmailAddress>());
-      expect(EmailAddress(faker.internet.safeEmail()), isA<EmailAddress>());
+    test("Should call all validators", () {
+      // arrange
+      final emailValidator = EmailValidatorMock();
+      final fakeEmail = faker.internet.email();
+      when(() => emailValidator.call(any())).thenReturn(null);
+
+      // act
+      final result = EmailAddress(
+        fakeEmail,
+        validators: [emailValidator],
+      );
+      // assert
+      expect(result, null);
+      verify(() => emailValidator.call(fakeEmail)).called(1);
     });
     test(
         "Should returns true if two emails are equals and false if its are different",
