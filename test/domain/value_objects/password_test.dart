96a7dd4089b0c31e0163d8d22259e3deff5819bd
Yann <iamyanndias@gmail.com>
2021-10-03 14:56:13 -0300

    ensure Password comparing returns tre if two instances are equal

--- test/domain/value_objects/password_test.dart
+++ test/domain/value_objects/password_test.dart
@@ -4,23 +4,13 @@ import 'package:vitrine/domain/value_objects/password.dart';
 
 void main() {
   group("Password", () {
-    test(
-        "Should throws a ValidationError.tooShort if password is shorter than 8 characters",
-        () {
-      expect(() => Password("1234"), throwsA(ValidationError.tooShort));
-    });
-    test("Should returns an instance if password lenght has 8 characters", () {
-      expect(Password("12345678"), isA<Password>());
-    });
+    test("Should returns true if two instances have the same value", () {
+      final one = Password("12345678");
+      final two = Password("12345678");
+      final three = Password("123456789");
 
-    test(
-        "Should returns an instance if password lenght is longer than 8 characters",
-        () {
-      expect(Password("123456789#"), isA<Password>());
-    });
-
-    test("Should throws a ValidationError.empty if password is empty", () {
-      expect(() => Password(""), throwsA(ValidationError.empty));
+      expect(one == two, true);
+      expect(one == three, false);
     });
   });
 }
