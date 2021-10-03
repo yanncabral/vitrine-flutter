a1a0b7dd827e153cf89c1569f016cf91b36fd89b
Yann <iamyanndias@gmail.com>
2021-10-03 14:57:41 -0300

    add const constructor

--- lib/validation/not_empty_validator.dart
+++ lib/validation/not_empty_validator.dart
@@ -2,6 +2,7 @@ import 'package:vitrine/domain/validation/validation_error.dart';
 import 'package:vitrine/domain/validation/validator.dart';
 
 class NotEmptyValidator implements Validator<String> {
+  const NotEmptyValidator();
   @override
   ValidationError? call(String value) {
     if (value.isEmpty) {
