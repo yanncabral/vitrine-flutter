f917796b656e60fd365063e77c005bf06d464ae2
Yann <iamyanndias@gmail.com>
2021-10-03 20:25:59 -0300

    add emailAlreadyInUse

--- lib/infra/firebase_errors.dart
+++ lib/infra/firebase_errors.dart
@@ -4,6 +4,8 @@ import 'package:vitrine/domain/error/domain_error.dart';
 extension FirebaseAuthDomainException on FirebaseAuthException {
   DomainError toDomain() {
     switch (code) {
+      case "email-already-in-use":
+        return DomainError.emailAlreadyInUse;
       case "invalid-email":
         return DomainError.invalidEmail;
       case "user-disabled":
