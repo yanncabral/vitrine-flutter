33e16c416b80ed80786a58029c210ae089aa09b8
Yann <iamyanndias@gmail.com>
2021-10-03 12:10:11 -0300

    ensure usecase returns DomainError

--- lib/domain/usecases/login_with_email_and_password_usecase.dart
+++ lib/domain/usecases/login_with_email_and_password_usecase.dart
@@ -1,6 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:vitrine/domain/error/domain_error.dart';
 import 'package:vitrine/domain/value_objects/email_address.dart';
 import 'package:vitrine/domain/value_objects/password.dart';
 
 abstract class LoginWithEmailAndPasswordUsecase {
-  void loginWith({required EmailAddress email, required Password password});
+  Either<DomainError, void> loginWith(
+      {required EmailAddress email, required Password password});
 }
