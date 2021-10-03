b4e939bafbaca167423d8e3ee64078b7ad8a1f3c
Yann <iamyanndias@gmail.com>
2021-10-03 15:39:17 -0300

    add formError stream in SignInViewModel

--- lib/domain/view_models/signin_view_model.dart
+++ lib/domain/view_models/signin_view_model.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import 'package:vitrine/domain/error/domain_error.dart';
 import 'package:vitrine/domain/validation/validation_error.dart';
 import 'package:vitrine/domain/value_objects/email_address.dart';
 import 'package:vitrine/domain/value_objects/password.dart';
@@ -6,6 +7,7 @@ import 'package:vitrine/domain/value_objects/password.dart';
 abstract class SignInViewModel {
   Stream<Either<ValidationError, EmailAddress>?> get emailState;
   Stream<Either<ValidationError, Password>?> get passwordState;
+  Stream<DomainError?> get formError;
   Stream<bool> get isLoadingState;
   Stream<bool> get isFormValidState;
 
