e5dd9d58f216234cca102f2139faed375bf71ff0
Yann <iamyanndias@gmail.com>
2021-10-03 14:25:11 -0300

    implement dependency injection in EmailAddress value object

--- lib/domain/value_objects/email_address.dart
+++ lib/domain/value_objects/email_address.dart
@@ -3,12 +3,13 @@ import 'package:vitrine/domain/value_objects/value_object.dart';
 import 'package:vitrine/validation/email_validator.dart';
 
 class EmailAddress extends ValueObject<String> {
-  EmailAddress(String data) : super(data) {
+  @override
+  final List<Validator> validators;
+
+  EmailAddress(String data, {this.validators = const [EmailValidator()]})
+      : super(data) {
     if (validationError != null) {
       throw validationError!;
     }
   }
-
-  @override
-  List<Validator> get validators => [EmailValidator()];
 }
