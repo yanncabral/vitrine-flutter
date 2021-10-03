d0aa269cf06c2dc64a8f20963ebbd916f333ac31
Yann <iamyanndias@gmail.com>
2021-10-03 14:25:52 -0300

    change SignInViewModelFactory use monostate pattern to return the same instance always

--- lib/main/factory/domain/view_models/signin_view_model_factory.dart
+++ lib/main/factory/domain/view_models/signin_view_model_factory.dart
@@ -2,5 +2,7 @@ import 'package:vitrine/domain/view_models/signin_view_model.dart';
 import 'package:vitrine/view_model/stream_controller/stream_controller_signin_view_model.dart';
 
 extension SignInViewModelFactory on SignInViewModel {
-  static SignInViewModel get factory => StreamControllerSignInViewModel();
+  static final _streamControllerSignInViewModel =
+      StreamControllerSignInViewModel();
+  static SignInViewModel get factory => _streamControllerSignInViewModel;
 }
