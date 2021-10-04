927cdfd52e26b7e47f55db5f1998e1f17bdf6a98
Yann <iamyanndias@gmail.com>
2021-10-03 21:31:05 -0300

    refactor FirebaseAuthenticationEnviroment

--- lib/infra/enviroment/firebase_authentication_enviroment.dart
+++ lib/infra/enviroment/firebase_authentication_enviroment.dart
@@ -3,34 +3,12 @@ import 'dart:async';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:vitrine/data/enviroment/authentication/authentication_enviroment.dart';
 
-class _AuthenticationEnviromentState {
-  AuthenticationState authenticationState = AuthenticationState.loggedOut;
-}
-
 class FirebaseAuthenticationEnviroment implements AuthenticationEnviroment {
-  final _state = _AuthenticationEnviromentState();
-  final _stateController =
-      StreamController<_AuthenticationEnviromentState>.broadcast();
-
   @override
-  Stream<AuthenticationState> get authenticationState => _stateController.stream
-      .map((state) => state.authenticationState)
+  Stream<AuthenticationState> get authenticationState => FirebaseAuth.instance
+      .authStateChanges()
+      .map((user) => user == null
+          ? AuthenticationState.loggedOut
+          : AuthenticationState.loggedIn)
       .distinct();
-
-  void setState(void Function() action) {
-    action();
-    _stateController.add(_state);
-  }
-
-  FirebaseAuthenticationEnviroment() {
-    FirebaseAuth.instance.authStateChanges().listen((user) {
-      setState(() {
-        if (user == null) {
-          _state.authenticationState = AuthenticationState.loggedOut;
-        } else {
-          _state.authenticationState = AuthenticationState.loggedIn;
-        }
-      });
-    });
-  }
 }
