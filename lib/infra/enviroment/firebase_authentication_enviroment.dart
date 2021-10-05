c936a2d8e5f092fc63c511c9cf784ab30ca51019
Yann <iamyanndias@gmail.com>
2021-10-05 12:17:20 -0300

    refactor authenticationState

--- lib/infra/enviroment/firebase_authentication_enviroment.dart
+++ lib/infra/enviroment/firebase_authentication_enviroment.dart
@@ -7,8 +7,10 @@ class FirebaseAuthenticationEnviroment implements AuthenticationEnviroment {
   @override
   Stream<AuthenticationState> get authenticationState =>
       FirebaseAuth.instance.authStateChanges().map((user) {
-        return user == null
-            ? AuthenticationState.loggedOut
-            : AuthenticationState.loggedIn;
+        if (user == null) {
+          return AuthenticationState.loggedOut;
+        } else {
+          return AuthenticationState.loggedIn;
+        }
       }).distinct();
 }
