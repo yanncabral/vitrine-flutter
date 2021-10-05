d090cce9aca5e0ded30ea5c78c925d142c8e9f86
Yann <iamyanndias@gmail.com>
2021-10-05 13:57:42 -0300

    add floating navbar to home

--- lib/ui/home_page/home_page.dart
+++ lib/ui/home_page/home_page.dart
@@ -1,10 +1,30 @@
+import 'package:floating_bottom_navigation_bar/floating_bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:vitrine/data/services/authentication/authentication_service.dart';
+import 'package:vitrine/ui/design/vanilla_color_scheme.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: FloatingNavbar(
+        backgroundColor: VanillaColorScheme.dark,
+        borderRadius: 30,
+        currentIndex: 0,
+        selectedBackgroundColor: null,
+        unselectedItemColor: VanillaColorScheme.medium,
+        selectedItemColor: VanillaColorScheme.secondary,
+        width: 303,
+        iconSize: 28,
+        onTap: (int index) {},
+        items: [
+          FloatingNavbarItem(icon: Icons.home),
+          FloatingNavbarItem(icon: Icons.search),
+          FloatingNavbarItem(icon: Icons.shopping_bag),
+          FloatingNavbarItem(icon: Icons.archive),
+          FloatingNavbarItem(icon: Icons.person),
+        ],
+      ),
       body: Center(
         child: TextButton(
           onPressed: () {
