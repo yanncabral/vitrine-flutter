128da164c0002e547f0b9964df10a6e95255cbc7
Yann <iamyanndias@gmail.com>
2021-10-06 07:26:42 -0300

    add VanillaTextField

---
+++ lib/ui/design/components/vanilla_text_field.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:vitrine/ui/design/vanilla_color_scheme.dart';
+
+class VanillaTextField extends StatelessWidget {
+  final String labelText;
+  final void Function(String text) onChange;
+  final String? errorText;
+  final bool? enabled;
+  final TextInputAction? textInputAction;
+  final TextInputType? keyboardType;
+
+  const VanillaTextField({
+    Key? key,
+    required this.labelText,
+    required this.onChange,
+    this.errorText,
+    this.enabled,
+    this.textInputAction,
+    this.keyboardType,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      enabled: enabled,
+      textInputAction: textInputAction,
+      onChanged: onChange,
+      keyboardType: keyboardType,
+      style: Theme.of(context).textTheme.caption?.copyWith(
+            color: VanillaColorScheme.black,
+          ),
+      cursorColor: VanillaColorScheme.dark,
+      decoration: InputDecoration(
+        disabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide(
+            color: VanillaColorScheme.light,
+            width: 0,
+          ),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide(
+            color: VanillaColorScheme.error,
+            width: 0,
+          ),
+        ),
+        errorText: errorText,
+        labelText: labelText,
+        labelStyle: Theme.of(context).textTheme.caption?.copyWith(
+              color: VanillaColorScheme.black.withOpacity(0.4),
+            ),
+        filled: true,
+        fillColor: VanillaColorScheme.light,
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide(
+            color: VanillaColorScheme.error,
+            width: 0,
+          ),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide(
+            color: VanillaColorScheme.light,
+            width: 0,
+          ),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide(
+            color: VanillaColorScheme.light,
+            width: 0,
+          ),
+        ),
+      ),
+    );
+  }
+}
