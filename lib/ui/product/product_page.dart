63ce42b723964b46f3a4c2cb9174f586e7842fb5
Yann <iamyanndias@gmail.com>
2021-10-06 04:58:20 -0300

    add product page

---
+++ lib/ui/product/product_page.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+import 'package:page_view_indicators/page_view_indicators.dart';
+import 'package:vitrine/ui/design/components/vanilla_action_button.dart';
+
+class ProductPage extends StatefulWidget {
+  @override
+  State<ProductPage> createState() => _ProductPageState();
+}
+
+class _ProductPageState extends State<ProductPage> {
+  final _pageController = PageController();
+  final _currentPageNotifier = ValueNotifier<int>(0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
+      extendBodyBehindAppBar: true,
+      body: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          PageView.builder(
+            controller: _pageController,
+            itemBuilder: (context, index) {
+              return backgroundImage();
+            },
+            onPageChanged: (int index) {
+              _currentPageNotifier.value = index;
+            },
+          ),
+          ProductPageInterface(
+            pageNotifier: _currentPageNotifier,
+            itemCount: 7,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container backgroundImage() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(
+            "assets/iphone.png",
+          ),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
+
+class ProductPageInterface extends StatelessWidget {
+  final ValueNotifier<int> pageNotifier;
+  final int itemCount;
+
+  const ProductPageInterface({
+    Key? key,
+    required this.pageNotifier,
+    required this.itemCount,
+  })  : assert(itemCount > 0),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(32),
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Colors.black,
+            Colors.transparent,
+          ],
+          begin: Alignment.bottomCenter,
+          end: Alignment.topCenter,
+        ),
+      ),
+      child: SafeArea(
+        top: false,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Apple",
+              style: Theme.of(context)
+                  .textTheme
+                  .headline4
+                  ?.copyWith(color: Colors.white.withOpacity(0.8)),
+            ),
+            const SizedBox(height: 8),
+            FractionallySizedBox(
+              widthFactor: 0.5,
+              child: Text(
+                "iPhone 11 Plus",
+                style: Theme.of(context)
+                    .textTheme
+                    .headline2
+                    ?.copyWith(color: Colors.white),
+              ),
+            ),
+            const SizedBox(height: 16),
+            Text(
+              "Descubra produtos que te merecem oferecidos por gente como a gente",
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyText1
+                  ?.copyWith(color: Colors.white.withOpacity(0.8)),
+            ),
+            const SizedBox(height: 32),
+            CirclePageIndicator(
+              currentPageNotifier: pageNotifier,
+              itemCount: itemCount,
+            ),
+            const SizedBox(height: 32),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: EdgeInsets.zero,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          "Ver loja",
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline4
+                              ?.copyWith(color: Colors.white),
+                        ),
+                        const Icon(
+                          Icons.chevron_right,
+                          color: Colors.white,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const Spacer(),
+                VanillaActionButton(
+                  title: "R\$1299,90",
+                  // title: "Pedir",
+                  onPressed: () => Navigator.of(context).pushNamed("/auth"),
+                  colorScheme: Brightness.light,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
