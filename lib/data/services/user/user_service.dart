7f678949eeb8576166a645501555ccd53c544c51
Yann <iamyanndias@gmail.com>
2021-10-06 06:15:10 -0300

    add UserService

---
+++ lib/data/services/user/user_service.dart
@@ -0,0 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dartz/dartz.dart';
+
+import 'package:vitrine/domain/entities/product.dart';
+import 'package:vitrine/domain/error/domain_error.dart';
+import 'package:vitrine/domain/usecases/add_product.dart';
+import 'package:vitrine/infra/models/firestore/firestore_product.dart';
+
+class UserService implements AddProduct {
+  @override
+  Future<Either<DomainError, Unit>> addProduct(Product product) async {
+    try {
+      FirebaseFirestore.instance
+          .collection("products")
+          .doc()
+          .set(product.toJson());
+      return const Right(unit);
+    } catch (e) {
+      return const Left(DomainError.unexpected);
+    }
+  }
+}
