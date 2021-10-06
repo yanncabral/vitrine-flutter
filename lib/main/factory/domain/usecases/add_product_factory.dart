e4b5dfc5d913df1d2c342e1ba64a5c9ebf57d4b3
Yann <iamyanndias@gmail.com>
2021-10-06 06:16:24 -0300

    add AddProductFactory

---
+++ lib/main/factory/domain/usecases/add_product_factory.dart
@@ -0,0 +1,7 @@
+import 'package:vitrine/data/services/user/user_service.dart';
+import 'package:vitrine/domain/usecases/add_product.dart';
+
+extension AddProductFactory on AddProduct {
+  static final _addProduct = UserService();
+  static AddProduct get factory => _addProduct;
+}
