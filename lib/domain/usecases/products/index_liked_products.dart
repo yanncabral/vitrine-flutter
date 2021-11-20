87558a17ab5036eb5022455f6e640015d87e3cd0
Yann <iamyanndias@gmail.com>
2021-11-20 12:06:01 -0300

    add IndexLikedProductsUsecase

---
+++ lib/domain/usecases/products/index_liked_products.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:vitrine/domain/entities/product.dart';
+import 'package:vitrine/domain/entities/user.dart';
+import 'package:vitrine/domain/error/domain_error.dart';
+
+abstract class IndexLikedProductsUsecase {
+  Future<Either<DomainError, List<Product>>> find(User user);
+}
