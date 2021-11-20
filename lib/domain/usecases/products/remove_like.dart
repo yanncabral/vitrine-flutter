9f6743a2dfd3dab435a06b2688eeb7bda653ff50
Yann <iamyanndias@gmail.com>
2021-11-20 12:07:04 -0300

    add remove like usecase

--- lib/domain/usecases/products/remove_like.dart
+++ lib/domain/usecases/products/remove_like.dart
@@ -3,6 +3,6 @@ import 'package:vitrine/domain/entities/product.dart';
 import 'package:vitrine/domain/entities/user.dart';
 import 'package:vitrine/domain/error/domain_error.dart';
 
-abstract class LikeProductUsecase {
-  Future<Either<DomainError, void>> like(Product product, User user);
+abstract class RemoveLikeProductUsecase {
+  Future<Either<DomainError, void>> removeLike(Product product, User user);
 }
